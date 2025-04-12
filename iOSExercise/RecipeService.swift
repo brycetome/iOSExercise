6969dbfa90ad703afbde426eba3407495d49df11
Bryce Tome <bryceptome@gmail.com>
2025-04-12 18:37:32 -0500

    Cache images and test cache images

--- iOSExercise/RecipeService.swift
+++ iOSExercise/RecipeService.swift
@@ -8,36 +8,26 @@
 import Foundation
 
 public struct RecipeResponse: Codable {
-    var recipes: [RecipeData]
-}
-
-public struct RecipeData: Codable, Equatable {
-    var cuisine: String
-    var name: String
-    var photo_url_large: String?
-    var photo_url_small: String?
-    var uuid: String
-    var source_url: String?
-    var youtube_url: String?
+    var recipes: [Recipe]
 }
 
 var AllRecipes: String = "https://d3jbb8n5wk0qxi.cloudfront.net/recipes.json"
 var MalformedRecipes: String = "https://d3jbb8n5wk0qxi.cloudfront.net/recipes-malformed.json"
 var EmptyData: String = "https://d3jbb8n5wk0qxi.cloudfront.net/recipes-empty.json"
 
-func fetchAllRecipes() async throws -> [RecipeData] {
+public func fetchAllRecipes() async throws -> [Recipe] {
     return try await fetchRecipesFromURLString(urlString: AllRecipes)
 }
 
-func fetchMalformedRecipes() async throws -> [RecipeData] {
+public func fetchMalformedRecipes() async throws -> [Recipe] {
     return try await fetchRecipesFromURLString(urlString: MalformedRecipes)
 }
 
-func fetchEmptyRecipes() async throws -> [RecipeData] {
+public func fetchEmptyRecipes() async throws -> [Recipe] {
     return try await fetchRecipesFromURLString(urlString: EmptyData)
 }
 
-private func fetchRecipesFromURLString(urlString: String) async throws -> [RecipeData] {
+private func fetchRecipesFromURLString(urlString: String) async throws -> [Recipe] {
     guard let url = URL(string:  urlString) else {
         throw NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "All Recipes URL is invalid"])
     }
