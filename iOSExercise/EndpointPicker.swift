c5d52299a57d7ab2e9c5b46855a92e75fb408976
Bryce Tome <bryceptome@gmail.com>
2025-04-13 08:54:18 -0500

    Select endpoint Error message Clean ups

---
+++ iOSExercise/EndpointPicker.swift
@@ -0,0 +1,18 @@
+//
+//  EndpointPicker.swift
+//  iOSExercise
+//
+//  Created by Bryce Tome on 4/13/25.
+//
+
+import SwiftUI
+
+struct EndpointPicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EndpointPicker()
+}
